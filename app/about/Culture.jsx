3a91bbc2b7aa6c42225e9172ba4c5fd48435a9c5
Piratha <piratha250392@yahoo.com>
2024-05-06 11:29:12 +0530

    ok

--- app/about/Culture.jsx
+++ app/about/Culture.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LearnImg from "../imgs/company/Learn.svg";
+import LearnImg from "../imgs/company/learn.svg";
 import Image from 'next/image';
 const Culture = () => {
     return (
